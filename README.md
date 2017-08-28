be8fe4fc23df5166e54f22a0effe41b23223e576
jfpoilpret <jfpoilpret@gmail.com>
2017-08-28 23:19:20 +0200

    First commit: initial extension setup

---
+++ README.md
@@ -0,0 +1,58 @@
+# vscode-fastarduino README
+
+This is the README for "vscode-fastarduino" extension. This extension makes it easy to work on Arduino (or AVR MCU) projects based on [FastArduino](https://github.com/jfpoilpret/fast-arduino-lib) library. It is also used to work on fastArduino library itself.
+
+## Features
+
+TODO
+
+Describe specific features of your extension including screenshots of your extension in action. Image paths are relative to this README file.
+
+For example if there is an image subfolder under your extension project workspace:
+
+\!\[feature X\]\(images/feature-x.png\)
+
+> Tip: Many popular extensions utilize animations. This is an excellent way to show off your extension! We recommend short, focused animations that are easy to follow.
+
+## Requirements
+
+FastArduino is based on C++, hence this extension needs the following VSCode extensions installed:
+- C/C++
+- C/C++ Clang Command Adapter
+
+In addition, I use the following extensions but they are not mandatory:
+- Include Autocomplete
+- TODO Highlight
+
+## Extension Settings
+
+TODO
+
+Include if your extension adds any VS Code settings through the `contributes.configuration` extension point.
+
+For example:
+
+This extension contributes the following settings:
+
+* `myExtension.enable`: enable/disable this extension
+* `myExtension.thing`: set to `blah` to do something
+
+## Known Issues
+
+Calling out known issues can help limit users opening duplicate issues against your extension.
+
+## Release Notes
+
+Users appreciate release notes as you update your extension.
+
+### 1.0.0
+
+Initial release of ...
+
+### 1.0.1
+
+Fixed issue #.
+
+### 1.1.0
+
+Added features X, Y, and Z.
---
+++ src/extension.ts
@@ -0,0 +1,98 @@
+'use strict';
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+
+// Internal structure holding important definitions for one board
+//TODO for new Makefile, we'll need more stuuf here (variant, mcu, arch, freq, default programmer...)
+//TODO for frequency we need to allow a range e.g. 1-20MHz
+class Board {
+    constructor(public readonly label: string, public readonly config: string, public readonly frequency: number[]) {}
+}
+
+// TODO later add more specific stuff here?
+class Target {
+    constructor(public readonly config: string) {}
+}
+
+// Internal map of all supported targets
+const allBoards: { [key: string]: Board; } = {
+    "Arduino UNO": new Board("Arduino UNO", "UNO-Release", [16]),
+    "Arduino NANO": new Board("Arduino NANO", "NANO-Release", [16]),
+    "Arduino LEONARDO": new Board("Arduino LEONARDO", "LEONARDO-Release", [16]),
+    "Arduino MEGA": new Board("Arduino MEGA", "MEGA-Release", [16]),
+    "ATmega328": new Board("ATmega328", "ATmega328-%{FREQ}-Release", [8,16]),
+    "ATtinyX4": new Board("ATtinyX4", "ATtinyX4-Release", [8])
+};
+
+//TODO need additional commands for EEPROM & fuses upload (fuses must be defined in workspace settings)
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+    //TODO initialize defaults (target, serial, programmer...)
+    //TODO use user-defined workspace settings for defaults?
+
+    // Register all commands
+    context.subscriptions.push(vscode.commands.registerCommand('extension.setBoard', () => {
+        setBoard(context);
+    }));
+    context.subscriptions.push(vscode.commands.registerCommand('extension.setSerial', () => {
+        // TODO
+        vscode.window.showInformationMessage('Set serial');
+    }));
+    context.subscriptions.push(vscode.commands.registerCommand('extension.setProgrammer', () => {
+        // TODO
+        vscode.window.showInformationMessage('Set programmer');
+    }));
+
+    //TODO register a TaskProvider
+    context.subscriptions.push(vscode.workspace.registerTaskProvider('make', {
+        provideTasks() {
+            return createTasks(context);
+        },
+        resolveTask(task: vscode.Task) {
+            return undefined;
+        }
+    }));
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {
+}
+
+// Internal implementation
+//=========================
+async function createTasks(context: vscode.ExtensionContext): Promise<vscode.Task[]> {
+    //TODO Check current directory has a Makefile
+    //TODO Get current target, serial...
+    const target: Target = context.workspaceState.get('fastarduino.target');
+    
+    //TODO Build several Tasks: Build, Clean, Flash, Eeprom, Fuses
+    let allTasks: vscode.Task[] = [];
+    let task: vscode.Task;
+    let command: string = `make CONF=${target.config} `;
+
+    task = new vscode.Task({ type: "fastarduino"}, "FastArduino: build", "fastarduino", new vscode.ShellExecution(command + "build", { cwd: "" }));
+    //TODO need to set problem matcher and presentation...
+    allTasks.push(task);
+
+    return allTasks;
+}
+
+async function setBoard(context: vscode.ExtensionContext) {
+    // Ask user to pick one target
+    const boardSelection = await vscode.window.showQuickPick(Object.keys(allBoards), { placeHolder: "Select target board or MCU" });
+    const board = allBoards[boardSelection];
+    let config = board.config;
+    // Ask for frequency if not fixed
+    if (board.frequency.length > 1) {
+        let listFrequencies: string[] = board.frequency.map<string>((f: number) => { return f.toString() + "MHz"});
+        const frequencySelection = await vscode.window.showQuickPick(listFrequencies, { placeHolder: "Select target MCU frequency" });
+        vscode.window.showInformationMessage(frequencySelection);
+        //TODO improve setup of config to ensure frequency is added at the right palce i.e. inside config string...
+        config = config.replace("%{FREQ}", frequencySelection);
+    }
+    vscode.window.showInformationMessage(config);
+    //TODO Store somewhere for use by other commands
+    context.workspaceState.update('fastarduino.target', new Target(config));
+}
